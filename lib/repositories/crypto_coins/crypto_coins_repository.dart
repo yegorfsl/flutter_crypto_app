b6189ff59737ba0e3d9c2ccd8764ec8155a84691
Yehor Farenbrukh <yehorgqt@gmail.com>
2023-09-04 00:18:45 +0200

    code_style_small_fix

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -9,15 +9,14 @@ class CryptoCoinsRepository {
 
     final data = response.data as Map<String, dynamic>;
     final dataRaw = data['RAW'] as Map<String, dynamic>;
-    final parsedData = dataRaw.entries
-        .map((e) {
+    final parsedData = dataRaw.entries.map((e) {
       final usdData = (e.value as Map<String, dynamic>)['USD'] as Map<String, dynamic>;
       final price = usdData['PRICE'];
       final imageUrl = usdData['IMAGEURL'];
       return CryptoCoin(
-      name: e.key,
-      priceInUSD: price,
-      imageUrl: 'https://www.cryptocompare.com/$imageUrl',
+        name: e.key,
+        priceInUSD: price,
+        imageUrl: 'https://www.cryptocompare.com/$imageUrl',
       );
     }).toList();
 
