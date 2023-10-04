5e197f23a4c6506288b47849726122c99399d9a5
Yehor Farenbrukh <yehorgqt@gmail.com>
2023-10-04 16:56:09 +0200

    crypto_coin_screen_layout

--- lib/features/crypto_coin/view/crypto_coin_screen.dart
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -2,11 +2,18 @@ import 'dart:developer';
 
 import 'package:crypto_list_first_app/features/crypto_coin/bloc/crypto_coin_bloc.dart';
 import 'package:crypto_list_first_app/repositories/crypto_coins/crypto_coins.dart';
+import 'package:crypto_list_first_app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 class CryptoCoinScreen extends StatefulWidget {
-  const CryptoCoinScreen({super.key});
+  const CryptoCoinScreen({
+    super.key
+    // required this.coin,
+  });
+
+  // final CryptoCoin coin;
 
   @override
   State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
@@ -17,7 +24,7 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
     GetIt.I<AbstractCoinsRepository>()
   );
 
-  late CryptoCoin coin = const CryptoCoin(name: 'name', priceInUSD: 1233, imageUrl: 'imageUrl');
+  late CryptoCoin coin = const CryptoCoin(name: 'BTC', priceInUSD: 1233, imageUrl: 'imageUrl');
 
   @override
   void initState() {
@@ -42,6 +49,75 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(coin.name)),
+      body: BlocBuilder<CryptoCoinBloc, CryptoCoinState>(
+        bloc: _cryptoCoinBloc,
+        builder: (context, state) {
+          if (state is CryptoCoinLoaded) {
+            return Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50),
+                  SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.network(coin.imageUrl),
+                  ),
+                  const SizedBox(height: 24),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 35),
+                      Text(
+                        coin.name,
+                        style: const TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w700
+                        ),
+                      ),
+                      Text(
+                        '${coin.priceInUSD} \$',
+                        style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700
+                        ),
+                      ),
+                    ]
+                  ),
+                ],
+              ),
+            );
+          }
+          if (state is CryptoCoinLoadingFailure) {
+            return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                        'Oops, something went wrong =(',
+                        style: darkTheme.textTheme.headlineMedium
+                    ),
+                    Text(
+                        'Please, try again later',
+                        style: darkTheme.textTheme.labelSmall?.copyWith(fontSize: 16)
+                    ),
+                    const SizedBox(height: 30),
+                    OutlinedButton(
+                        onPressed: () {
+                          _cryptoCoinBloc.add(LoadCryptoCoinDetails(coin: coin.name));
+                        },
+                        child: const Text('Try again')
+                    )
+                  ],
+                )
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
